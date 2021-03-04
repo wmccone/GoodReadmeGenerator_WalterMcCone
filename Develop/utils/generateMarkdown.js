176986d3a775bbd2983df2adbcae8ec9d5638a70
Gerry M <waltermccone88@gmail.com>
2021-03-04 17:54:57 -0600

    added a swith function to render the license link function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,7 +18,21 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) { 
+  const licenseLink = ""
+  switch (license) {
+    case "MIT":
+      licenseLink = "https://opensource.org/licenses/MIT"
+      break;
+    case "GPL":
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0"
+      break;
+    case "Apache":
+      licenseLink = "https://opensource.org/licenses/Apache-2.0"
+      break;
+  }
+  return licenseLink
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
